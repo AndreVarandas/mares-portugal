ed25665644061b0eaefda7cbd9aed1b4e51de13c
André Varandas <andre.m.varandas@gmail.com>
2023-11-07 16:54:23 +0000

    fix build errors, update title

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,29 +1,31 @@
 import "./bootstrap";
 
-// Check for geolocation and set user's location
-if ("geolocation" in navigator) {
-    // Check if the user location is set
+(async () => {
+    // Check for geolocation and set user's location
+    if ("geolocation" in navigator) {
+        // Check if the user location is set
 
-    const position = await new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject);
-    });
+        const position = await new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(resolve, reject);
+        });
 
-    const { latitude: userLatitude, longitude: userLongitude } =
-        position.coords;
+        const { latitude: userLatitude, longitude: userLongitude } =
+            position.coords;
 
-    const CSRF_TOKEN = document
-        .querySelector('meta[name="csrf-token"]')
-        .getAttribute("content");
+        const CSRF_TOKEN = document
+            .querySelector('meta[name="csrf-token"]')
+            .getAttribute("content");
 
-    fetch("/set-user-location", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "X-CSRF-TOKEN": CSRF_TOKEN,
-        },
-        body: JSON.stringify({
-            userLatitude,
-            userLongitude,
-        }),
-    });
-}
+        fetch("/set-user-location", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-CSRF-TOKEN": CSRF_TOKEN,
+            },
+            body: JSON.stringify({
+                userLatitude,
+                userLongitude,
+            }),
+        });
+    }
+})();
