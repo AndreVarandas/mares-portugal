6f6056b371b3464a364f2890c6e23cbab5c89425
André Varandas <andre.m.varandas@gmail.com>
2023-11-08 15:10:35 +0000

    feat: adds analytics#

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,6 +14,9 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
+    <!-- Analytics -->
+    <script defer data-domain="mares.andrevarandas.dev" src="https://analytics.varandas.io/js/script.js"></script>
+
     <!-- Styles -->
     @vite('resources/css/app.css')
 </head>
