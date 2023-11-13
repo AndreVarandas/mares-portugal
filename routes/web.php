a3c10daeb3844d12b7bb9d82a425108c99ca1cba
André Ferreira <ajmnbf@gmail.com>
2023-11-13 16:41:26 +0000

    Bug Fix route

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [TidesController::class, 'index'])->name('tides');
-Route::view('/moons', [MoonsController::class, 'index'])->name('moons');
+Route::get('/moons', [MoonsController::class, 'index'])->name('moons');
