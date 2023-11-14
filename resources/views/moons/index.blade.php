60a2cd2201ec5112ca8eafd02293d97d4603b0e2
André Varandas <andre.m.varandas@gmail.com>
2023-11-14 10:00:14 +0000

    minor UI update

--- resources/views/moons/index.blade.php
+++ resources/views/moons/index.blade.php
@@ -6,13 +6,16 @@
 
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-6 mt-8">
             @foreach ($moonData as $moon)
-                <div class="bg-white dark:bg-gray-800 shadow-md md:rounded-md p-6">
+                <div class="bg-white flex items-center dark:bg-gray-800 shadow-md md:rounded-md p-6">
                     <figure class="mb-2 text-6xl">
                         {{ $moon['emoji'] }}
                     </figure>
-                    <h2 class="text-xl font-bold text-gray-800 dark:text-white">{{ $moon['desc_en'] }}
-                    </h2>
-                    <p class="text-gray-600 font-thin dark:text-gray-400">{{ $moon['day'] }}</p>
+                    <div class="flex flex-col space-y-2 ml-4">
+                        <h2 class="text-xl font-bold text-gray-800 dark:text-white">{{ $moon['desc_en'] }}
+                        </h2>
+                        <p class="text-gray-600 font-thin dark:text-gray-400">{{ $moon['day'] }}</p>
+                    </div>
+
                 </div>
             @endforeach
         </div>
